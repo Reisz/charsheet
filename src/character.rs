c16eb021593fc5935ef1ffbd6d94231ab2ca7f6c
Reisz <leinad.ffafp@gmail.com>
2020-01-25 23:27:04 +0100

    Move behavioural code to Character.

--- src/character.rs
+++ src/character.rs
@@ -7,29 +7,55 @@ pub struct Character<'a> {
     items: Vec<u16>,
 }
 
-impl ValueStorage for Character<'_> {
-    fn get(&self, id: ValueId) -> i32 {
+impl Character<'_> {
+    /// Get a value
+    pub fn get(&self, id: ValueId) -> i32 {
         self.values[id.idx()]
     }
 
-    fn set(&mut self, id: ValueId, value: i32) {
+    /// Change a value
+    pub fn set(&mut self, id: ValueId, value: i32) {
         let old = self.values[id.idx()];
         if value == old {
             return;
         }
 
         self.values[id.idx()] = value;
-        self.model.update_dependents(id, self);
+        self.update_dependents(id);
 
         // TODO: update observers
         // NOTE: group values, only make groups observable
     }
-}
 
-impl Character<'_> {
     /// Add an item to the character.
     pub fn equip(&mut self, id: ItemId) {
         self.items[id.idx()] += 1;
-        self.model.apply_modifications(id, self);
+        self.apply_modifications(id);
+    }
+
+    fn update_dependents(&mut self, id: ValueId) {
+        for dependent in &self.model.values[id.idx()].dependents {
+            let mut value = self.model.values[dependent.idx()].base;
+
+            for (factor, dependency) in &self.model.values[dependent.idx()].dependencies {
+                value += (factor * self.get(*dependency) as f32) as i32;
+            }
+
+            self.set(*dependent, value);
+        }
+    }
+
+    fn apply_modifications(&mut self, item: ItemId) {
+        for (id, modification) in &self.model.items[item.idx()].modifications {
+            let value = self.get(*id);
+            self.set(
+                *id,
+                match modification {
+                    Modification::Add(summand) => value + summand,
+                    Modification::Multiply(factor) => (value as f32 * factor) as i32,
+                    Modification::Change(value) => *value,
+                },
+            );
+        }
     }
 }
--- src/model.rs
+++ src/model.rs
@@ -34,10 +34,10 @@ pub struct Value {
     /// Front end data
     pub front_end: FrontEnd,
 
-    base: i32,
+    pub(crate) base: i32,
 
-    dependencies: Vec<(f32, ValueId)>,
-    dependents: Vec<ValueId>,
+    pub(crate) dependencies: Vec<(f32, ValueId)>,
+    pub(crate) dependents: Vec<ValueId>,
 }
 
 /// Represents all the ways a Value can be modified by an Item.
@@ -56,24 +56,16 @@ pub struct Item {
     /// Front end data
     pub front_end: FrontEnd,
 
-    modifications: Vec<(ValueId, Modification)>,
+    pub(crate) modifications: Vec<(ValueId, Modification)>,
 }
 
 /// Contains a set of values and items that can be used together.
 pub struct Model {
-    values: Vec<Value>,
-    items: Vec<Item>,
+    pub(crate) values: Vec<Value>,
+    pub(crate) items: Vec<Item>,
 
     value_ids: HashMap<String, ValueId>,
     item_ids: HashMap<String, ItemId>,
-}
-
-/// Allows procedures in Model to manipulate values.
-pub trait ValueStorage {
-    /// Get a value.
-    fn get(&self, id: ValueId) -> i32;
-    /// Change a value
-    fn set(&mut self, id: ValueId, value: i32);
 }
 
 impl Model {
@@ -107,33 +99,5 @@ impl Model {
     /// Get the ItemId corresponding to an id string.
     pub fn item_id(&self, id: &str) -> ItemId {
         self.item_ids[id]
-    }
-
-    /// Update all values that depend on a changed value.
-    pub fn update_dependents<S: ValueStorage>(&self, id: ValueId, storage: &mut S) {
-        for dependent in &self.values[id.idx()].dependents {
-            let mut value = self.values[dependent.idx()].base;
-
-            for (factor, dependency) in &self.values[dependent.idx()].dependencies {
-                value += (factor * storage.get(*dependency) as f32) as i32;
-            }
-
-            storage.set(*dependent, value);
-        }
-    }
-
-    /// Apply new modifications to a value after an item has been equipped.
-    pub fn apply_modifications<S: ValueStorage>(&self, item: ItemId, storage: &mut S) {
-        for (id, modification) in &self.items[item.idx()].modifications {
-            let value = storage.get(*id);
-            storage.set(
-                *id,
-                match modification {
-                    Modification::Add(summand) => value + summand,
-                    Modification::Multiply(factor) => (value as f32 * factor) as i32,
-                    Modification::Change(value) => *value,
-                },
-            );
-        }
     }
 }
