330a6e89b39c5651b67226308d0bec097be7168d
Reisz <Reisz@users.noreply.github.com>
2020-01-28 17:43:20 +0100

    Add missing docs.

--- src/model/item.rs
+++ src/model/item.rs
@@ -11,20 +11,31 @@ pub enum Modification {
     Change(i32),
 }
 
+/// Input value for an item condition.
 pub enum ConditionInput {
+    /// Compare against a constant
     Const(i32),
+    /// Compare against a character value
     Value(f32, ValueId),
 }
 
+/// Comparison operator for coditions
 pub enum ConditionOperator {
+    /// Both sides have to be equal.
     Eq,
+    /// Both sides can not be equal.
     Ne,
+    /// A is less than or equal to B.
     Le,
+    /// A is strictly less than B.
     Lt,
+    /// A is more than or equal to B.
     Ge,
+    /// A is strictly equal to B.
     Gt,
 }
 
+/// Condition controlling activation of an item.
 pub struct Condition {
     pub(crate) op: ConditionOperator,
     pub(crate) a: ConditionInput,
@@ -32,6 +43,7 @@ pub struct Condition {
 }
 
 impl Condition {
+    /// Create a new condition.
     pub fn new(a: ConditionInput, op: ConditionOperator, b: ConditionInput) -> Self {
         Self { a, op, b }
     }
@@ -58,6 +70,7 @@ impl Item {
         }
     }
 
+    /// Create a new item, which automatically applies itself based on the given condition.
     pub fn with_condition(front_end: FrontEnd, condition: Condition) -> Self {
         Self {
             front_end,
