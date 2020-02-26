6a18c7e168c5f1962ec7e4eb7784a1ee92ddfb66
Reisz <Reisz@users.noreply.github.com>
2020-02-26 16:42:48 +0100

    Choice: Add front end methods and documentation.

--- src/model/choice.rs
+++ src/model/choice.rs
@@ -1,20 +1,29 @@
-use std::collections::HashMap;
-use super::{ValueId, Modification, FrontEnd};
+use super::FrontEnd;
 
+/// Part of a Choice.
 pub struct Selection {
     pub(crate) front_end: Option<FrontEnd>,
-    pub(crate) modifications: HashMap<ValueId, Modification>,
+    // pub(crate) modifications: HashMap<ValueId, Modification>,
 }
 
+/// Represents a set of Selections. The Character will have to have exactly one Selection active at a time.
 pub struct Choice {
     pub(crate) front_end: Option<FrontEnd>,
-    pub(crate) options: Vec<Selection>,
+    // pub(crate) options: Vec<Selection>,
 }
 
 impl Selection {
-    
+    /// Add front end metadata.
+    pub fn front_end(mut self, front_end: FrontEnd) -> Self {
+        self.front_end = Some(front_end);
+        self
+    }
 }
 
 impl Choice {
-
+    /// Add front end metadata.
+    pub fn front_end(mut self, front_end: FrontEnd) -> Self {
+        self.front_end = Some(front_end);
+        self
+    }
 }
